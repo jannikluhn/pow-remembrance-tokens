08d19e660d3bd04f05719f0efb56587ba7264903
Jannik Luhn <jannik@brainbot.com>
2022-05-27 22:22:50 +0200

    Add validate task

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,5 +1,12 @@
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy");
+const { default: Common, Chain, Hardfork } = require("@ethereumjs/common");
+const { Block, BlockHeader } = require("@ethereumjs/block");
+const { BN } = require("ethereumjs-util");
+const { default: Ethash } = require("@ethereumjs/ethash");
+const level = require("level-mem");
+
+const cacheDB = level();
 
 task("mint", "Mint a token")
   .addParam("account", "minter account")
@@ -15,7 +22,7 @@ task("mint", "Mint a token")
     const contract = await ethers.getContract("PoWRemembranceToken", signer);
 
     const tx = await contract.mint(address, [
-      args.mixhash,
+      ethers.utils.zeroPad(args.mixhash, 32),
       parseInt(args.nonce),
     ]);
     console.log("minting... ", tx);
@@ -40,6 +47,57 @@ task("mintSome", "Mint some tokens for testing").setAction(async (args) => {
   await Promise.all(txPromises);
 });
 
+task("validate", "").setAction(async (args) => {
+  const { deployer } = await hre.getNamedAccounts();
+  const signer = await ethers.getSigner(deployer);
+  const contract = await ethers.getContract("PoWRemembranceToken", signer);
+
+  const numTokens = await contract.totalSupply();
+  for (let i = 0; i < numTokens; i++) {
+    const tokenID = await contract.tokenByIndex(i);
+    const [owner, pow, validated] = await Promise.all([
+      contract.ownerOf(tokenID),
+      contract.pows(tokenID),
+      contract.validated(tokenID),
+    ]);
+    if (validated) {
+      console.log("token", tokenID, "already validated");
+    } else {
+      console.log("validating token", tokenID.toString(), "...");
+      if (await validatePoW(owner, pow)) {
+        console.log("token is valid, marking...");
+        const tx = await contract.validate(tokenID);
+        await tx.wait();
+        console.log("done");
+      } else {
+        console.log("token", tokenID, "is invalid");
+      }
+    }
+  }
+});
+
+async function validatePoW(coinbase, pow) {
+  const e = new Ethash(cacheDB);
+  // TODO: parent should be last PoW block
+
+  console.log({
+    difficulty: new BN(100),
+    number: new BN(1),
+    coinbase: coinbase,
+    nonce: ethers.utils.zeroPad(pow.nonce.toHexString(), 8),
+    mixHash: pow.mixHash,
+  });
+  const block = Block.fromBlockData({
+    header: {
+      difficulty: new BN(100),
+      number: new BN(1),
+      coinbase: coinbase,
+      nonce: ethers.utils.zeroPad(pow.nonce.toHexString(), 8),
+      mixHash: pow.mixHash,
+    },
+  });
+  return await e.verifyPOW(block);
+}
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -60,5 +118,6 @@ module.exports = {
 
   namedAccounts: {
     deployer: 0,
+    miner: 1,
   },
 };
