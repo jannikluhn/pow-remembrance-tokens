3331dea24a7053d85c2587408e376f5fb5c5b110
Jannik Luhn <jannik@brainbot.com>
2022-05-27 17:58:32 +0200

    Add task to mint token

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy");
 
+task("mint", "Mint a token")
+  .addParam("account", "minter account")
+  .addParam("mixhash", "mixhash value")
+  .addParam("nonce", "nonce value")
+  .setAction(async (args) => {
+    const namedAccounts = await hre.getNamedAccounts();
+    const address = namedAccounts[args.account];
+    if (!address) {
+      throw new Error("Unknown account " + args.account);
+    }
+    const signer = await ethers.getSigner(address);
+    const contract = await ethers.getContract("POWRemembranceToken", signer);
+
+    const tx = await contract.mint(address, [
+      args.mixhash,
+      parseInt(args.nonce),
+    ]);
+    console.log("minting... ", tx);
+    const receipt = await tx.wait();
+    console.log(
+      "successfully minted token",
+      receipt.events[0].args.tokenId.toString()
+    );
+  });
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
